ac51ef974fd814cc1b4edfa1d32ca511c87f95ab
JuliaZaykaS <sys55@mail.ru>
2021-04-07 15:38:23 +0300

    7 зад 4 модуль

--- test.js
+++ test.js
@@ -189,11 +189,11 @@
 
 // function calculateMeanTemperature(forecast) {
 
-  //   const {today: {low:todayLow, high:todayHigh}, tomorrow: {low:tomorrowLow, high:tomorrowHigh}} = forecast;
-  //   // Пиши код выше этой строки
-  //   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
-  // }
-  // console.log(calculateMeanTemperature({ today: {low: 28, high: 32}, tomorrow: {low: 25, high: 29} }));
+//   const {today: {low:todayLow, high:todayHigh}, tomorrow: {low:tomorrowLow, high:tomorrowHigh}} = forecast;
+//   // Пиши код выше этой строки
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
+// console.log(calculateMeanTemperature({ today: {low: 28, high: 32}, tomorrow: {low: 25, high: 29} }));
 
 
 //   function makeTask(data) {
@@ -229,61 +229,121 @@
 //   // Пиши код выше этой строки
 // };
 
-const atTheOldToad = {
-  potions: [
-    { name: 'Зелье скорости', price: 460 },
-    { name: 'Дыхание дракона', price: 780 },
-    { name: 'Каменная кожа', price: 520 },
-  ],
-  // Пиши код ниже этой строки
-  getPotions() {
-    return this.potions;
-  },
+// const atTheOldToad = {
+//   potions: [
+//     { name: 'Зелье скорости', price: 460 },
+//     { name: 'Дыхание дракона', price: 780 },
+//     { name: 'Каменная кожа', price: 520 },
+//   ],
+//   // Пиши код ниже этой строки
+//   getPotions() {
+//     return this.potions;
+//   },
 
-  addPotion(newPotion) {
-    for (let potion of this.potions) {
+//   addPotion(newPotion) {
+//     for (let potion of this.potions) {
 
-      if (potion.name === newPotion.name) {
-        return `Зелье ${newPotion.name} уже есть в инвентаре!`;
-      }
-    }
-    this.potions.push(newPotion);
+//       if (potion.name === newPotion.name) {
+//         return `Зелье ${newPotion.name} уже есть в инвентаре!`;
+//       }
+//     }
+//     this.potions.push(newPotion);
 
-  },
-  removePotion(potionName) {
-for (let i = 0; i < this.potions.length; i+=1) {
-  if (this.potions[i].name === potionName) {
+//   },
+//   removePotion(potionName) {
+// for (let i = 0; i < this.potions.length; i+=1) {
+//   if (this.potions[i].name === potionName) {
 
-    this.potions.splice(i, 1);
+//     this.potions.splice(i, 1);
+//   }
+
+// }
+// return `Зелья ${potionName} нет в инвентаре!`;
+
+
+
+//   },
+
+
+//   updatePotionName(oldName, newName) {
+//     for (let i = 0; i < this.potions.length; i+=1) {
+//       if (this.potions[i].name === oldName) {
+//         this.potions[i].name = newName;
+
+//       }
+
+//     }
+//       return `Зелья ${oldName} нет в инвентаре!`;
+
+
+//   },
+//   // Пиши код выше этой строки
+// };
+// console.log(atTheOldToad.getPotions());
+// console.log(atTheOldToad.addPotion({ name: 'Невидимка', price: 620 }));
+// console.log(atTheOldToad.removePotion('Дыхание дракона'));
+// console.log(atTheOldToad);
+// console.log(atTheOldToad.updatePotionName('Каменная кожа', 'Зелье неуязвимости'));
+// console.log(atTheOldToad);
+
+
+// const pizzaPalace = {
+//   pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+//   order(pizzaName, onSuccess, onError) {
+
+//     // for (let i = 0; i < pizzaPalace.pizzas.length; i += 1) {
+//     //   // console.log(pizzaPalace.pizzas);
+//     //   if (pizzaPalace.pizzas[i] === pizzaName) {
+//     //     // console.log(pizzaPalace.pizzas[i]);
+//     //     return onSuccess(pizzaName);
+//     //   };
+//     // }
+//     // return onError(pizzaName);
+//     if (!pizzaPalace.pizzas.includes(pizzaName)) {
+//       return onError(pizzaName);
+//     }
+//     return onSuccess(pizzaName);
+//   },
+// };
+// // Пиши код выше этой строки
+
+// // Колбэк для onSuccess
+// function makePizza(pizzaName) {
+//   return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+// }
+
+// // Колбэк для onError
+// function onOrderError(error) {
+//   return `Ошибка! В ассортименте нет пиццы с названием ${error}`;
+// }
+
+// // Вызовы метода с колбэками
+// console.log(pizzaPalace.order('Аль Копчино', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Четыре нарезона', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Биг майк', makePizza, onOrderError));
+// console.log(pizzaPalace.order('Венская', makePizza, onOrderError));;
+
+const orders = [
+  { email: 'solomon@topmail.ua', dish: 'Burger' },
+  { email: 'artemis@coldmail.net', dish: 'Pizza' },
+  { email: 'jacob@mail.com', dish: 'Taco' },
+];
+
+// Пиши код ниже этой строки
+function composeMessage(position) {
+  // let message = `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position + 1}-й в очереди.`
+  // return message;
+  for (let i = 0; i < this.length; i += 1) {
+    // this[i].position = i + 1;
+    let position = i + 1;
+    // console.log(orders);
+    // let message[i] = `Готовим ${this.dish} для ${this.email}. Ваш заказ ${this.position}-й в очереди.`;
   }
-
-}
-return `Зелья ${potionName} нет в инвентаре!`;
-
-
-
-  },
-
-
-  updatePotionName(oldName, newName) {
-    for (let i = 0; i < this.potions.length; i+=1) {
-      if (this.potions[i].name === oldName) {
-        this.potions[i].name = newName;
-
-      }
-
-    }
-      return `Зелья ${oldName} нет в инвентаре!`;
-
-
-  },
-  // Пиши код выше этой строки
+  // return message[i];
+  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${this.position}-й в очереди.`
 };
-console.log(atTheOldToad.getPotions());
-console.log(atTheOldToad.addPotion({ name: 'Невидимка', price: 620 }));
-console.log(atTheOldToad.removePotion('Дыхание дракона'));
-console.log(atTheOldToad);
-console.log(atTheOldToad.updatePotionName('Каменная кожа', 'Зелье неуязвимости'));
-console.log(atTheOldToad);
 
-
+console.log(composeMessage.call(orders));
+// console.log(composeMessage());
+// const messages = orders.map(composeMessage.call(orders));
+// console.log(composeMessage.call(orders));
